28e6fcfcfdf62b2bfe5b807d761bfb65b3bc5f0f
Nandhakumar <nandhu_kumar86@yahoo.com>
2021-03-15 22:55:44 +0530

    Modified the inputs

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace espansoeditor
         static void Main(string[] args)
         {
 
-            if (args.Length < 2)
+            if (args.Length < 3)
             {
                 System.Console.WriteLine("Empty trigger or replacements!!!");
             }
             else
             {
-                string Path = @"/home/nandhakumar/Documents/MyGit/espansotriggers/user/nn.yml";
+                string Path = args[0];
 
                 using (var reader = new StreamReader(Path))
                 {
@@ -27,11 +27,11 @@ namespace espansoeditor
 
                     var triggerProp = new YamlScalarNode();
                     triggerProp.Style = YamlDotNet.Core.ScalarStyle.DoubleQuoted;
-                    triggerProp.Value = args[0];
+                    triggerProp.Value = args[1];
 
                     var replaceProp = new YamlScalarNode();
                     replaceProp.Style = YamlDotNet.Core.ScalarStyle.DoubleQuoted;
-                    replaceProp.Value = args[1];
+                    replaceProp.Value = args[2];
 
                     var espansoItem = new YamlMappingNode();
                     espansoItem.Add("trigger", triggerProp);
