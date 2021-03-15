78c99aa5e9a997b5cc1053317d2c355723fc3d2b
Nandhakumar <nandhu_kumar86@yahoo.com>
2021-03-15 22:29:31 +0530

    Initital Console checkin

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using YamlDotNet.RepresentationModel;
+
+namespace espansoeditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length < 2)
+            {
+                System.Console.WriteLine("Empty trigger or replacements!!!");
+            }
+            else
+            {
+                string Path = @"/home/nandhakumar/Documents/MyGit/espansotriggers/user/nn.yml";
+
+                using (var reader = new StreamReader(Path))
+                {
+                    var yaml = new YamlStream();
+                    yaml.Load(reader);
+
+                    var rootNode = (YamlMappingNode)yaml.Documents[0].RootNode;
+                    var matches = (YamlSequenceNode)rootNode["matches"];
+
+                    var triggerProp = new YamlScalarNode();
+                    triggerProp.Style = YamlDotNet.Core.ScalarStyle.DoubleQuoted;
+                    triggerProp.Value = args[0];
+
+                    var replaceProp = new YamlScalarNode();
+                    replaceProp.Style = YamlDotNet.Core.ScalarStyle.DoubleQuoted;
+                    replaceProp.Value = args[1];
+
+                    var espansoItem = new YamlMappingNode();
+                    espansoItem.Add("trigger", triggerProp);
+                    espansoItem.Add("replace", replaceProp);
+
+                    matches.Add(espansoItem);
+
+                    using (TextWriter writer = File.CreateText(Path))
+                    {
+                        yaml.Save(writer, false);
+                    }
+                }
+
+            }
+        }
+    }
+}
